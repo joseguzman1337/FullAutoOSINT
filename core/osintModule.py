f55cfe96cc1e7f5e1ad5961277d6570d385bd655
jose guzman <joseguzman1337@users.noreply.github.com>
2026-03-31 00:50:35 -0500

    [CRUSH] chore(repo): checkpoint pending changes #TASK-000

--- core/osintModule.py
+++
@@ -1,6 +0,0 @@
-from core.actionModule import actionModule
-
-class osintModule(actionModule):
-    def __init__(self, config, display, lock):
-        super(osintModule, self).__init__(config, display, lock)
-        self.safeLevel = 5
